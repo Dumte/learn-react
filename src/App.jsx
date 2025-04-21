dd0e03d82e9e5314c0babdb58ff07dc925009a74
dumte <dummy4real@gmail.com>
2025-04-21 22:07:19 +0100

    refactor: clean up App component by removing commented-out Counter code

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
-import Counter from './projects/counter/counter';
 import Todo from './projects/Todo/Todo';
 
 const App = () => {
    
   return (
     <div className='flex justify-center items-center flex-col gap-4 bg-blue-200 h-screen'>
-      {/* <Counter /> */}
-
-      {/* <div>=================</div> */}
-
-      <Todo />
+           <Todo />
     </div>
   );
 }
